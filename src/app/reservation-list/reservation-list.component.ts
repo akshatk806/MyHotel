1b37aa36dd9186dcf330eed89b4ca9a02ea39e02
akshatkumar <akshatk806@gmail.com>
2025-01-27 20:34:35 +0530

    showing all the reservations in list and using ng-template

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
   selector: 'app-reservation-list',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  reservations: Reservation[] = [];
 
+  // Angular DI injects reservation service into this component
+  constructor(private reservationService : ReservationService) {   
+
+  }
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getAllReservations();
+  }
 }
